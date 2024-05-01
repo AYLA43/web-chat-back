1058f7a888d3c6507b3bb0d2a7ddce7e265774a9
ayachh <164261954+AYLA43@users.noreply.github.com>
2024-05-02 00:28:22 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: true }));
+
+// main request
+app.get("/", async (req, res) => {
+    res.send(`Hello World`);
+});
+
+// authenticate request
+app.post("/authenticate", async (req, res) => {
+    // code
+    //   get username from body
+    const { username } = req.body;
+    //   get or create user on chat engine server
+    try {
+        const response = await axios.put(
+            "https://api.chatengine.io/users/",
+            {
+                username: username,
+                secret: username,
+                first_name: username,
+            },
+            {
+                headers: {
+                    "Private-Key": "9101bb43-30fb-43e0-bd78-57d8b87b5763",
+
+                },
+            }
+        );
+        return res.send(response.data);
+    } catch (e) {
+        return res.status(404).json(e.response.data);
+    }
+});
+
+app.listen(3000, () => {
+    console.log(`Server is Running on http://localhost:3000`);
+});
